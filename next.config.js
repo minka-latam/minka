a6775cb11f80af08aec83e2203dbbfc01b5c28af
Kurt Guardia <kurtguardia@gmail.com>
2025-10-29 19:37:55 +0400

    fix(donation) Add api.dicebear.com to nextconfig

--- next.config.js
+++ next.config.js
@@ -3,40 +3,45 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+        protocol: 'https',
+        hostname:
+          'hebbkx1anhila5yf.public.blob.vercel-storage.com',
       },
       {
-        protocol: "https",
-        hostname: "swfgvfhpmicwptupjyko.supabase.co",
+        protocol: 'https',
+        hostname: 'swfgvfhpmicwptupjyko.supabase.co',
       },
       {
-        protocol: "https",
-        hostname: "euflkzolctyfscsnwspq.supabase.co",
+        protocol: 'https',
+        hostname: 'euflkzolctyfscsnwspq.supabase.co',
       },
       {
-        protocol: "https",
-        hostname: "noplbukuwhiizabgmfoo.supabase.co",
+        protocol: 'https',
+        hostname: 'noplbukuwhiizabgmfoo.supabase.co',
       },
       {
-        protocol: "https",
-        hostname: "aoovlhsyzfrfbouqcedw.supabase.co",
+        protocol: 'https',
+        hostname: 'aoovlhsyzfrfbouqcedw.supabase.co',
       },
       {
-        protocol: "https",
-        hostname: "img.youtube.com",
+        protocol: 'https',
+        hostname: 'img.youtube.com',
       },
       {
-        protocol: "https",
-        hostname: "i.ytimg.com",
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
       },
       {
-        protocol: "https",
-        hostname: "source.unsplash.com",
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
       },
     ],
   },
@@ -46,6 +51,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   // ... other config options
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
